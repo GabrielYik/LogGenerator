18b2ff2979e544db8d4b508a497410cc4ee94aa9
Gabriel Yik Ren Jun <gabrielyik@yahoo.com.sg>
2019-12-14 21:15:40 +0800

    Add ability to choose different scenario files

--- src/main/java/logen/App.java
+++ src/main/java/logen/App.java
@@ -1,35 +1,77 @@
-package loggenerator;
+package logen;
+
+import static logen.storage.Config.FILE_EXTENSION;
+import static logen.storage.Config.LOG_DIR_PATH;
+import static logen.storage.Config.SCENARIO_DIR_PATH;
+import static logen.util.PathUtil.SCENARIO_FILE_FILTER;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import loggenerator.model.Log;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
+
+import logen.model.Log;
+import logen.storage.Scenario;
+import logen.util.PathUtil;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
-    public static final String SCENARIO_FILE_PATH = "." + File.separator + "many_failed_login_attempts.json";
-    public static final String CSV_FILE_PATH = "." + File.separator + "logs.csv";
-
     public static void main(String[] args) throws IOException {
-        Scenario scenario = readScenario();
+        List<String> scenariosFileNames = fetchScenariosFileNames();
+        displayScenarioChoices(scenariosFileNames);
+        int scenarioChoice = getScenarioChoice();
+        Scenario scenario = readScenario(scenariosFileNames.get(scenarioChoice - 1));
+
         LogGenerator logGenerator = new LogGenerator(scenario);
         List<Log> logs = logGenerator.generate();
+
         List<String> headers = scenario.getHeaders();
         writeAsCsv(headers, logs);
     }
 
-    private static Scenario readScenario() throws IOException {
+    private static List<String> fetchScenariosFileNames() throws IOException {
+        Path scenarioDirPath = new File(SCENARIO_DIR_PATH).toPath();
+        List<String> scenarios = new ArrayList<>();
+        Files.newDirectoryStream(scenarioDirPath, SCENARIO_FILE_FILTER)
+            .forEach(p -> scenarios.add(PathUtil.toFileName(p)));
+        return scenarios;
+    }
+
+    private static void displayScenarioChoices(List<String> scenariosFileNames) {
+        int currentListing = 1;
+        for (int i = currentListing; i <= scenariosFileNames.size(); i++) {
+            System.out.println(
+                "Scenario "
+                    + i
+                    + ": "
+                    + scenariosFileNames.get(currentListing - 1));
+        }
+    }
+
+    private static int getScenarioChoice() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    private static Scenario readScenario(String scenarioFileName) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        File scenarioFile = new File(SCENARIO_FILE_PATH);
+        File scenarioFile = new File(SCENARIO_DIR_PATH + scenarioFileName + FILE_EXTENSION);
         return objectMapper.readValue(scenarioFile, Scenario.class);
     }
 
     private static void writeAsCsv(List<String> headers, List<Log> logs) throws IOException {
-        FileWriter writer = new FileWriter(CSV_FILE_PATH);
+        if (!Files.exists(Paths.get(LOG_DIR_PATH))) {
+            Files.createDirectory(Paths.get(LOG_DIR_PATH));
+        }
+        FileWriter writer = new FileWriter(LOG_DIR_PATH + "log.csv");
         CSVPrinter printer = new CSVPrinter(writer, CSVFormat.EXCEL.withHeader(headers.toArray(new String[]{})));
         logs.forEach(log -> {
             try {
---
+++ src/main/java/logen/storage/Config.java
@@ -0,0 +1,19 @@
+package logen.storage;
+
+import java.io.File;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Path;
+
+public class Config {
+    private static final String ROOT = ".";
+    private static final String SCENARIO_DIR_NAME = "scenarios";
+    public static final String SCENARIO_FILE_PREFIX = "scenario";
+
+    public static final String SCENARIO_DIR_PATH = ROOT + File.separator + SCENARIO_DIR_NAME + File.separator;
+    public static final String LOG_DIR_PATH = ROOT + File.separator + "logs" + File.separator;
+
+    public static final String FILE_EXTENSION = ".json";
+
+    public static final DirectoryStream.Filter<Path> scenarioFileFilter =
+        p -> p.getFileName().toString().substring(0, 8).equals(SCENARIO_FILE_PREFIX);
+}
