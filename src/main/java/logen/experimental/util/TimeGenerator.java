da85075e7cfd59473ed9bce31f96650d71959a99
Gabriel Yik Ren Jun <gabrielyik@yahoo.com.sg>
2020-05-23 16:31:59 +0800

    Introduce different specialisations of time generator

--- src/main/java/logen/experimental/util/TimeGenerator.java
+++ src/main/java/logen/experimental/util/TimeGenerator.java
@@ -2,20 +2,21 @@ package logen.experimental.util;
 
 import logen.experimental.scenario.TimePeriod;
 
+import java.sql.Time;
 import java.time.LocalTime;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class TimeGenerator {
+    private static int SECONDS_IN_MINUTE = 60;
+    private static int SECONDS_IN_HOUR = 3600;
+
     private final LocalTime earliestTime;
     private final LocalTime latestTime;
     private final int approximateRequiredGenerations;
 
-    public TimeGenerator(TimePeriod timePeriod, int approximateRequiredGenerations) {
-        this.earliestTime = timePeriod.getStartTime();
-        this.latestTime = timePeriod.getEndTime();
-        this.approximateRequiredGenerations = approximateRequiredGenerations;
-    }
+    private LocalTime base;
 
-    public TimeGenerator(
+    private TimeGenerator(
             LocalTime earliestTime,
             LocalTime latestTime,
             int approximateRequiredGenerations
@@ -25,7 +26,41 @@ public class TimeGenerator {
         this.approximateRequiredGenerations = approximateRequiredGenerations;
     }
 
+    public static TimeGenerator bounded(
+            LocalTime earliestTime,
+            LocalTime latestTime,
+            int approximateRequiredGenerations
+    ) {
+        return new TimeGenerator(earliestTime, latestTime, approximateRequiredGenerations);
+    }
+
+    public static TimeGenerator forward(LocalTime earliestTime) {
+        return new TimeGenerator(earliestTime, null, -1);
+    }
+
+    public static TimeGenerator back(LocalTime latestTime) {
+        return new TimeGenerator(null, latestTime, -1);
+    }
+
     public LocalTime generate() {
-        return null;
+        long seconds = generateRandomSeconds(SECONDS_IN_MINUTE, SECONDS_IN_HOUR);
+        boolean isBack = earliestTime == null && latestTime != null;
+        if (isBack) {
+            base = base.minusSeconds(seconds);
+        } else {
+            base = base.plusSeconds(seconds);
+        }
+        return base;
+    }
+
+    private static long generateRandomSeconds(long origin, long bound) {
+        return ThreadLocalRandom
+                .current()
+                .nextLong(origin, bound);
+    }
+
+
+    public void skip(int generationCount) {
+
     }
 }
