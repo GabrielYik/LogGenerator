af7af9ae17ce4bd3a278c8d7520457b5bbff2cae
Gabriel Yik Ren Jun <gabrielyik@yahoo.com.sg>
2020-05-29 23:44:21 +0800

    Implement setting of placeholder log count for unset placeholders

--- src/main/java/logen/experimental/generation/fixed/FixedLogGenerator.java
+++ src/main/java/logen/experimental/generation/fixed/FixedLogGenerator.java
@@ -29,20 +29,57 @@ public class FixedLogGenerator {
      * @return An fixture containing the fixed logs and placeholders.
      */
     public Fixture generate() {
+        List<GroupFixture> groupFixtures = applyGroupAttributes();
+        GroupFixture consolidated = consolidateGroupFixtures(groupFixtures);
+        GroupFixture completedGroupFixture = completeGroupFixtureConstruction(consolidated);
+        return constructFixture(completedGroupFixture);
+    }
+
+    /**
+     * Generates group fixtures by applying scenario group attributes
+     * that define fixed logs on the logs of a group.
+     *
+     * @return The group fixtures
+     */
+    private List<GroupFixture> applyGroupAttributes() {
         List<GroupFixture> groupFixtures = new ArrayList<>();
         for (Group group : scenario.getGroups()) {
             GroupFixture groupFixture = new GroupAttributesApplier(group).apply();
             groupFixtures.add(groupFixture);
         }
-        return constructFixture(groupFixtures);
-
+        return groupFixtures;
     }
 
-    public Fixture constructFixture(List<GroupFixture> groupFixtures) {
-        GroupFixture consolidatedGroupFixture = groupFixtures.stream()
+    /**
+     * Consolidate {@code groupFixtures} into a single group fixture.
+     *
+     * @param groupFixtures The group fixtures
+     * @return A consolidated group fixture from {@code groupFixtures}
+     */
+    private GroupFixture consolidateGroupFixtures(List<GroupFixture> groupFixtures) {
+        return groupFixtures.stream()
                 .reduce(GroupFixture.empty(), GroupFixture::merge);
-        List<Log> fixedLogs = consolidatedGroupFixture.getFixedLogs();
-        List<Placeholder> placeholders = consolidatedGroupFixture.getPlaceholders().stream()
+    }
+
+    /**
+     * Completes the construction of {@code groupFixture}.
+     *
+     * @param groupFixture A mostly constructed group fixture
+     * @return A completely constructed group fixture
+     */
+    private GroupFixture completeGroupFixtureConstruction(GroupFixture groupFixture) {
+        return new PlaceholderLogCountSetter(scenario, groupFixture).set();
+    }
+
+    /**
+     * Constructs a fixture from {@code groupFixture}.
+     *
+     * @param groupFixture The group fixture
+     * @return A fixture
+     */
+    private Fixture constructFixture(GroupFixture groupFixture) {
+        List<Log> fixedLogs = groupFixture.getFixedLogs();
+        List<Placeholder> placeholders = groupFixture.getPlaceholders().stream()
                 .map(Placeholder.Builder::build)
                 .collect(Collectors.toList());
         return new Fixture(fixedLogs, placeholders);
---
+++ src/main/java/logen/experimental/generation/fixed/PlaceholderLogCountSetter.java
@@ -0,0 +1,178 @@
+package logen.experimental.generation.fixed;
+
+import logen.experimental.scenario.Scenario;
+import logen.experimental.util.RandomChooser;
+import logen.experimental.util.timegenerators.TimeGenerator;
+import logen.experimental.util.timegenerators.UnboundedTimeGenerator;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * An allocator that allocates a number of logs to placeholders of type
+ * {@code FLEXIBLE}.
+ * <p>
+ * The preprocessor has to be executed before the generation of filler logs.
+ */
+public class PlaceholderLogCountSetter {
+    private final Scenario scenario;
+    private final GroupFixture groupFixture;
+    private final List<Placeholder.Builder> placeholders;
+
+    /**
+     * Constructs a placeholder log count setter from {@code scenario}
+     * and {@code groupFixture}.
+     *
+     * @param scenario A scenario
+     * @param groupFixture A group fixture
+     */
+    public PlaceholderLogCountSetter(
+            Scenario scenario,
+            GroupFixture groupFixture
+    ) {
+        this.scenario = scenario;
+        this.groupFixture = groupFixture;
+        placeholders = groupFixture.getPlaceholders();
+    }
+    /**
+     * Sets the number of logs that a placeholder of type {@code FLEXIBLE}
+     * specifies.
+     * If the placeholder is not of type {@code FLEXIBLE}, no change will
+     * be made to the placeholder.
+     *
+     * @return The group fixture with all placeholders having their log
+     *   count set
+     */
+    public GroupFixture set() {
+        setLogCountForInnerPlaceholders();
+        setLogCountForOuterPlaceholders();
+        return groupFixture;
+    }
+
+    private void setLogCountForInnerPlaceholders() {
+        for (int i = 1; i < placeholders.size() - 1; i++) {
+            Placeholder.Builder placeholder = placeholders.get(i);
+            if (!placeholder.getType().equals(PlaceholderType.FLEXIBLE)) {
+                continue;
+            }
+
+            TimeGenerator timeGenerator = UnboundedTimeGenerator.forward(
+                    placeholder.getStartTime(),
+                    scenario.getTimePeriod().getEndTime(),
+                    scenario.getTimePeriod().getStartTime()
+            );
+            int logCount = computeLogCountForInnerPlaceholder(placeholder, timeGenerator);
+            placeholder.withLogCount(logCount);
+        }
+    }
+
+    private int computeLogCountForInnerPlaceholder(
+            Placeholder.Builder placeholder,
+            TimeGenerator timeGenerator
+    ) {
+        int logCount = 0;
+        LocalTime time = timeGenerator.generate();
+        while (time.isBefore(placeholder.getEndTime())) {
+            logCount++;
+            time = timeGenerator.generate();
+        }
+        return logCount;
+    }
+
+    private void setLogCountForOuterPlaceholders() {
+        int logCount = computeLogCountForOuterPlaceholders();
+        distributeOverOuterPlaceholders(logCount);
+    }
+
+    private int computeLogCountForOuterPlaceholders() {
+        int logCount = computeRemainingLogCount();
+        if (hasNoneLeft(logCount)) {
+            return scenario.getLogCount() / 3;
+        } else if (hasNotEnough(logCount)) {
+            return logCount / 3 + logCount;
+        } else {
+            return logCount;
+        }
+    }
+
+    /**
+     * Checks if there are any logs left.
+     *
+     * @param logCount The number of logs
+     * @return True if there are logs left, false otherwise
+     */
+    private boolean hasNoneLeft(int logCount) {
+        return logCount <= 0;
+    }
+
+    /**
+     * Checks if the number of logs is not enough.
+     *
+     * @param logCount The number of logs
+     * @return True if there are not enough logs left,
+     *   false otherwise
+     */
+    private boolean hasNotEnough(int logCount) {
+        int sufficiencyFloor = scenario.getLogCount() / 3;
+        return logCount < sufficiencyFloor;
+    }
+
+    private int computeRemainingLogCount() {
+        return scenario.getLogCount() - computeCurrentLogCount();
+    }
+
+    private int computeCurrentLogCount() {
+        return groupFixture.getFixedLogs().size() + (int) placeholders.stream()
+                .filter(placeholder -> placeholder.getType().equals(PlaceholderType.FLEXIBLE))
+                .map(Placeholder.Builder::getLogCount)
+                .count();
+    }
+
+    /**
+     * Distributes {@code logCount} randomly over the two outer
+     * placeholders.
+     *
+     * @param logCount The number of logs to distribute
+     */
+    private void distributeOverOuterPlaceholders(int logCount) {
+        List<Integer> distribution = randomlyDivide(logCount);
+        Placeholder.Builder firstPlaceholder = placeholders.get(0);
+        firstPlaceholder.withLogCount(distribution.get(0));
+        Placeholder.Builder lastPlaceholder = placeholders.get(placeholders.size() - 1);
+        lastPlaceholder.withLogCount(distribution.get(1));
+    }
+
+    /**
+     * Randomly divide {@code logCount} into two parts.
+     *
+     * @param logCount The number of logs to distribute
+     */
+    private List<Integer> randomlyDivide(int logCount) {
+        int quotient = logCount / 2;
+        int remainder = logCount - quotient;
+        List<Integer> values = new ArrayList<>();
+        values.add(quotient);
+        values.add(remainder);
+        
+        int from = 0;
+        int to = 1;
+        int counter = 1;
+        while (counter != 2) {
+            int randomValue = RandomChooser.chooseBetween(1, values.get(from));
+
+            int fromValue = values.get(from);
+            int toValue = values.get(to);
+            fromValue -= randomValue;
+            toValue += randomValue;
+            values.set(from, fromValue);
+            values.set(to, toValue);
+
+            from = (from + 1) % 2;
+            to = (to + 1) % 2;
+            counter++;
+        }
+
+        return values;
+    }
+}
