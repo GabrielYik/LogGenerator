edb07525b51d6d39eb36d1902d14d4cfd9cd5aef
Gabriel Yik Ren Jun <gabrielyik@yahoo.com.sg>
2020-05-30 16:05:54 +0800

    Extract out list randomisation into random util

--- src/main/java/logen/experimental/util/RandomUtil.java
+++ src/main/java/logen/experimental/util/RandomUtil.java
@@ -3,8 +3,9 @@ package logen.experimental.util;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Random;
+import java.util.function.BiFunction;
 
-public class RandomChooser {
+public class RandomUtil {
     private static final Random rng;
 
     static {
@@ -28,10 +29,40 @@ public class RandomChooser {
         return start + rng.nextInt(end + 1);
     }
 
+    public static long chooseBetween(long start, long end) {
+        return start + rng.nextLong() % end;
+    }
+
     public static LocalTime chooseBetween(LocalTime startTime, LocalTime endTime) {
         int startTimeSeconds = startTime.toSecondOfDay();
         int endTimeSeconds = endTime.toSecondOfDay();
         int deltaSeconds = endTimeSeconds - startTimeSeconds;
         return LocalTime.ofSecondOfDay(startTimeSeconds + deltaSeconds);
+    }
+
+    public static <E> List<E> randomise(
+            List<E> distribution,
+            BiFunction<Integer, E, E> choice,
+            BiFunction<E, E, E> subtraction,
+            BiFunction<E, E, E> addition
+    ) {
+        int from = 0;
+        int to = 1;
+        int counter = 1;
+        while (counter != distribution.size()) {
+            E randomValue = choice.apply(1, distribution.get(from));
+
+            E fromValue = distribution.get(from);
+            E toValue = distribution.get(to);
+            fromValue = subtraction.apply(fromValue, randomValue);
+            toValue = addition.apply(toValue, randomValue);
+            distribution.set(from, fromValue);
+            distribution.set(to, toValue);
+
+            from = (from + 1) % 2;
+            to = (to + 1) % 2;
+            counter++;
+        }
+        return distribution;
     }
 }
